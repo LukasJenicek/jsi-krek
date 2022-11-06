6d8070a5b17b2b73d4b5e986d42c38b541d6d23b
lukasjenicek <you@example.com>
2022-11-06 21:21:50 +0100

    Serving http server on specific port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+)
+
+type Server struct{}
+
+func (*Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte{})
+}
 
 func main() {
-	fmt.Printf("wat")
+	handler := Server{}
+	addr := ":9099"
+
+	server := http.Server{
+		Addr:    addr,
+		Handler: &handler,
+	}
+
+	log.Printf("serving on port %s", addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
