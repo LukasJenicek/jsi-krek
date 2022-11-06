40866735deff17db602c6fcf4be162b6ecd03cbd
lukasjenicek <you@example.com>
2022-11-06 21:38:49 +0100

    Configure zlog as a structured logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	stdlog "log"
 	"net/http"
-	"log"
+	"os"
+	"time"
+
 	"github.com/LukasJenicek/jsi-krek/services/api"
+	"github.com/rs/zerolog"
+	zlog "github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -14,10 +19,35 @@ func main() {
 		Handler: api.Router(),
 	}
 
-	log.Printf("serving on port %s", addr)
+	configureLogger()
+
+	zlog.Info().Msgf("serving on port %s", addr)
 
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+}
+
+// TODO: Move to setup functionality
+func configureLogger() {
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().
+		Caller().
+		Timestamp().
+		Logger()
+
+	loc := time.Local
+	zerolog.TimestampFunc = func() time.Time { return time.Now().In(loc) }
+	zerolog.MessageFieldName = "msg"
+	zerolog.TimeFieldFormat = "15:04:05"
+	zerolog.CallerSkipFrameCount = 3
+
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	// Redirect stdlib's log output to zerolog.
+	stdlog.SetFlags(0)
+	stdlog.SetOutput(logger)
+	stdlog.SetPrefix("")
+
+	zlog.Logger = logger
 }
